a715be20d061eb914e31fcc2b94b7b32fb40e236
JoaoGuilhermeBatista <joaoguilhermebatistamoura@gmail.com>
2026-04-06 16:28:25 -0300

    Feat: Adiciona o Repository dentro de Tarefas e Funcionarios

---
+++ src/main/java/dev/java10x/cadastrodefuncionarios/Tarefas/TarefasRepoistory.java
@@ -0,0 +1,5 @@
+package dev.java10x.cadastrodefuncionarios.Tarefas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefasRepoistory extends JpaRepository<TarefasModel, Long> {
+}
